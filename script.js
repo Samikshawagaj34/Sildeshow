10dd261cfb72f075228be04b289e59061fb6d940
Samiksha Wagaj <samikshawagaj2003@gmail.com>
2024-11-06 13:11:18 +0530

    page modified

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ let index = 0;
 const slide = document.getElementById('slide');
 function next()
 {
+    if(index < images.length -1){
+
+    
    index++;
+}
+else{
+    index = 0;
+}
    slide.src = images[index]
 
 
